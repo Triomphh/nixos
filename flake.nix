7afe0c7346769af3b8419d5ed94f16a9368a726f
Triomph <worker13prime@gmail.com>
2025-05-25 15:18:30 +0200

    Add system architecture to flake system 'desktop'

--- flake.nix
+++ flake.nix
@@ -26,6 +26,7 @@
 
     # Desktop
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/desktop/configuration.nix
