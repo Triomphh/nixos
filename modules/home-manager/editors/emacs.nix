e0bc74e429f6bf1442311fc93afd27be0f8a4f8d
Triomph <worker13prime@gmail.com>
2025-05-27 19:39:06 +0200

    Add vterm package to Emacs

--- modules/home-manager/editors/emacs.nix
+++ modules/home-manager/editors/emacs.nix
@@ -4,7 +4,12 @@
 	programs.emacs = {
 		enable = true;
 		package = pkgs.emacs;
+
+		extraPackages = epkgs: with epkgs; [
+			vterm
+		];
 	};
+
 
 	# Starts the Emacs daemon
 	services.emacs.enable = true;
