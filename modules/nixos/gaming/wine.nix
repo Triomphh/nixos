fa56de8012f61b64682a70ef82883a385dc0709f
Triomph <worker13prime@gmail.com>
2025-06-13 23:41:55 +0200

    Change to `Full` variant of wine

--- modules/nixos/gaming/wine.nix
+++ modules/nixos/gaming/wine.nix
@@ -2,7 +2,7 @@
 
 {
   environment.systemPackages = with pkgs; [
-    wineWowPackages.stable    # Wine with both 32-bit and 64-bit support
-    winetricks      # Essential Wine configuration tool
+    wineWowPackages.stableFull
+    winetricks
   ];
-} 
+}
