e6925bd4548c554e604225f6f5f6603726914007
Triomph <worker13prime@gmail.com>
2025-09-20 13:23:34 +0200

    Modify `switch-to-workspace-x` keybinds

--- modules/home-manager/desktop/gnome.nix
+++ modules/home-manager/desktop/gnome.nix
@@ -100,6 +100,11 @@
       close = ["<Super>q"];
       # toggle fullscreen
       toggle-fullscreen = ["<Alt>Return"];
+      # switch to workspaces
+      switch-to-workspace-1 = ["<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
     };
     
     # Window management settings
