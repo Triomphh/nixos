5d68c919e8ffce34971f8b61481c2106f49ddf75
Triomph <worker13prime@gmail.com>
2026-05-02 16:53:45 +0200

    add zed settings

--- modules/home-manager/editors/zed-editor.nix
+++ modules/home-manager/editors/zed-editor.nix
@@ -18,9 +18,101 @@
 
       theme = lib.mkForce {
         mode = "system";
-        light = "One Light";
-        dark = "One Dark";
+        light = "Catppuccin Latte (Blur) [Light]";
+        dark = "Catppuccin Frappé (Blur) [Light]";
+      };
+
+      centered_layout = {
+        left_padding = 0.27;
+        right_padding = 0.27;
+      };
+
+      profiles = {
+        Zen = {
+          tab_bar = {
+            show = false;
+            show_nav_history_buttons = false;
+          };
+          toolbar = {
+            breadcrumbs = false;
+            quick_actions = false;
+          };
+          gutter = {
+            # folds = false;
+          };
+          indent_guides = {
+            enabled = false;
+          };
+          scrollbar = {
+            show = "never";
+            cursors = false;
+            git_diff = false;
+            search_results = false;
+            selected_symbol = false;
+          };
+          status_bar = {
+            "experimental.show" = false;
+          };
+        };
       };
     };
+
+    userKeymaps = [
+      {
+        bindings = {
+          "ctrl-alt-super-c" = "workspace::ToggleCenteredLayout";
+          "ctrl-shift-e" = "project_panel::Toggle";
+          "ctrl-?" = "agent::Toggle";
+        };
+      }
+      {
+        context = "(VimControl && !menu) || (!Editor && !Terminal)";
+        bindings = {
+          "shift-left" = "workspace::ActivatePaneLeft";
+          "shift-right" = "workspace::ActivatePaneRight";
+          "shift-up" = "workspace::ActivatePaneUp";
+          "shift-down" = "workspace::ActivatePaneDown";
+        };
+        unbind = {
+          "ctrl-w h" = "workspace::ActivatePaneLeft";
+          "ctrl-w l" = "workspace::ActivatePaneRight";
+          "ctrl-w k" = "workspace::ActivatePaneUp";
+          "ctrl-w j" = "workspace::ActivatePaneDown";
+        };
+      }
+      {
+        context = "Editor && vim_mode == insert";
+        bindings = {};
+      }
+      {
+        context = "VimControl && !menu";
+        unbind = {
+          "shift-h" = "vim::WindowTop";
+          "shift-l" = "vim::WindowBottom";
+          "ctrl-j" = "vim::Down";
+        };
+      }
+      {
+        context = "Workspace";
+        bindings = {
+          "ctrl-alt-super-z" = [
+            "action::Sequence"
+            [
+              # "workspace::ToggleCenteredLayout"
+              "settings_profile_selector::Toggle"
+              ["workspace::SendKeystrokes" "Z e n enter"]
+            ]
+          ];
+          "ctrl-alt-super-shift-z" = [
+            "action::Sequence"
+            [
+              # "workspace::ToggleCenteredLayout"
+              "settings_profile_selector::Toggle"
+              ["workspace::SendKeystrokes" "D i s a b l e d enter"]
+            ]
+          ];
+        };
+      }
+    ];
   };
 }
