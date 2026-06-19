a16ec8d850b9df45fe3672dd154df11a80787aa7
Triomph <worker13prime@gmail.com>
2026-06-19 23:02:37 +0200

    open Proton VPN on startup

--- modules/nixos/network/protonvpn.nix
+++ modules/nixos/network/protonvpn.nix
@@ -4,4 +4,14 @@
   environment.systemPackages = with pkgs; [
     proton-vpn
   ];
+
+  systemd.user.services.protonvpn = {
+    description = "Proton VPN GUI";
+    wantedBy = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.proton-vpn}/bin/protonvpn-app";
+      Restart = "on-failure";
+    };
+  };
 }
